000af2da31814f8eebee5b82689ecd73ab3f5dd5
Adil Heybetov <heybetov1998@gmail.com>
2017-03-06 18:59:17 +0400

    text editor yuklendi

---
+++ text/js/script.js
@@ -0,0 +1,16 @@
+function take() {
+    a = document.getElementById('text');
+}
+
+function bold() {
+    a.style.fontWeight = "bold";
+}
+
+function italic() {
+    a.style.fontStyle = "italic";
+}
+
+function sizeTake() {
+    b = document.getElementById('size').value;
+    a.style.fontSize = b+"px";
+}
