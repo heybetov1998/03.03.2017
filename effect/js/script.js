cc5f20cde8684b4123457b79541ae8bc8f4f3bc1
Adil Heybetov <heybetov1998@gmail.com>
2017-03-06 20:03:37 +0400

    effect yuklendi

---
+++ effect/js/script.js
@@ -0,0 +1,30 @@
+function one() {
+    document.getElementById('shkl').style.filter ="blur(5px)";
+}
+function two() {
+    document.getElementById('shkl').style.filter="brightness(200%)";
+}
+function three() {
+document.getElementById('shkl').style.filter="contrast(200%)";
+}
+function five() {
+document.getElementById('shkl').style.filter="grayscale(100%)";
+}
+function six() {
+document.getElementById('shkl').style.filter="hue-rotate(90deg)";
+}
+function seven() {
+document.getElementById('shkl').style.filter="invert(100%)";
+}
+function eight() {
+document.getElementById('shkl').style.filter="opacity(30%)";
+}
+function nine() {
+document.getElementById('shkl').style.filter="saturate(8)";
+}
+function ten() {
+document.getElementById('shkl').style.filter="sepia(100%)";
+}
+function eleven() {
+document.getElementById('shkl').style.filter="none";
+}
